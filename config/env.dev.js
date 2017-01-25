eeb23676666515821ad8956ad36426194b734c84
A92hm <ali92hm@gmail.com>
2017-01-26 00:25:52 +1100

    added info

--- config/env.dev.js
+++ config/env.dev.js
@@ -1,5 +1,5 @@
 // Use env.dev.js for environment variables that will be set when the server starts locally.
-// Use for your api keys, secrets, etc. This file should **not** and is not tracked by git.
+// Use for your api keys, secrets, etc. This file should **not** and is not tracked by git beyond this initial commit.
 //
 // You will need to set these parameters on the server you deploy to.
 
